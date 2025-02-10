1aefd18cee0e23bb5d3e9e1d31f0ce5e818d71e0
muqeet ur rehman <muqeetkhan050@gmail.com>
2025-02-10 22:23:52 +1000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {BrowserRouter,Router, Routes,Route} from 'react-router-dom';
 import './App.css'
 import Header from './Header'
 import Men from './Men'
@@ -8,15 +9,21 @@ import Women from './Women'
 import Kids from './kids'
 
 function App() {
+
+  const navigate=useNavigate();
   
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
     <Header />
       <h1 style={{fontSize:'190px', color:'#f4a007 '}}>FIND YOUR STYLE</h1>
       <Men />
       <Women />       
       <Kids />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
