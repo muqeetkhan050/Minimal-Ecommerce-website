f3c0e1923ecb4287a6da40fbb9a5663b080b1ec8
muqeet ur rehman <muqeetkhan050@gmail.com>
2025-02-13 23:22:17 +1000

    card added

--- src/MenPage.jsx
+++ src/MenPage.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import MenCard from './MenCard'
+
+
+const Mproducts=[
+    {id:1, name: 'T-shirt', price:20, image:'this is our image'},
+    {id:2, name: 'shirt', price:30, image:'this is our shirt image'},
+    {id:3, name: 'jackets', price:40, image:'this is our image for jackets'}
+];
 
 const MenPage=()=>{
     return(
         <>
-       thsi si csbdh
+       <div>Here are best shirts</div>
+       <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
+        {
+            Mproducts.map((product)=>(
+                <MenCard key={product.id} {...product}/>
+
+            ))
+        }
+       </div>
         </>
     )
 }
