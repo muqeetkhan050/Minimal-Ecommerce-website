5e91c43e75ee741c47d8c68c86882b51db901dc4
muqeet ur rehman <muqeetkhan050@gmail.com>
2025-02-13 23:51:24 +1000

    commit

--- src/MenCard.jsx
+++ src/MenCard.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const MenCard=({name,price, image})=>{
     return(
-        <>
-        <img src={image} alt={name} style={{ width: "100%", height: "200px", objectFit: "cover" }} />
-        <>{name}</>
-        <>${price}</>
-        </>
+        <div className="card shadow-sm border-0">
+        <img src={image} alt={name} style={{ height: "250px", objectFit: "cover" }}  />
+        <div className="card-body text-center">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text fw-bold text-success">${price}</p>
+        <button className="btn btn-primary">Add to Cart</button>
+        </div>
+        </div>
     )
 }
 export default MenCard
--- src/MenPage.jsx
+++ src/MenPage.jsx
@@ -3,24 +3,28 @@ import MenCard from './MenCard'
 
 
 const Mproducts=[
-    {id:1, name: 'T-shirt', price:20, image:'this is our image'},
-    {id:2, name: 'shirt', price:30, image:'this is our shirt image'},
-    {id:3, name: 'jackets', price:40, image:'this is our image for jackets'}
+    {id:1, name: 'T-shirt', price:20, image:'public/men1.jpg'},
+    {id:2, name: 'shirt', price:30, image:'public/men1.jpg'},
+    {id:3, name: 'jackets', price:40, image:'public/men1.jpg'}
 ];
 
 const MenPage=()=>{
     return(
-        <>
-       <div>Here are best shirts</div>
-       <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
+        <div className="container py-5">
+       <h1 className="text-center mb-4 fw-bold text-primary">All Clothes</h1>
+       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
         {
             Mproducts.map((product)=>(
-                <MenCard key={product.id} {...product}/>
+                <div className="col" key={product.id}>
+                <MenCard {...product}/>
+                </div>
 
             ))
+            
         }
+       
        </div>
-        </>
+        </div>
     )
 }
 
