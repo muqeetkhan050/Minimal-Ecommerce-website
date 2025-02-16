86b3f8e5d580f9238db06c25dda47cc001ef0584
muqeet ur rehman <muqeetkhan050@gmail.com>
2025-02-16 15:00:00 +1000

    kid card added

--- src/KidCard.jsx
+++ src/KidCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function KidCard({name, price , image}){
     return(
-        <>
-        
-        <img src={image} alt={name} style={{ width: "100%", height: "200px", objectFit: "cover" }} />
-        <>{name}</>
-        <>${price}</>
-        
-
-        </>
+        <div className="card border border-dark shadow-sm p-3" style={{ width: "100%", maxWidth: "300px" }}>
+        <img src={image} className="card-img-top rounded" alt={name} style={{ height: "250px", objectFit: "cover" }} />
+        <div className="card-body text-center">
+        <h5 className="card-title fw-bold">{name}</h5>
+        <p className="card-text fw-bold text-success">${price}</p>
+        <button className="btn btn-primary">Add to Cart</button>
+        </div>
+        </div>
     )
 
 }
